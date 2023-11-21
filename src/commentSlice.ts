8398f931af35c2869890d9995ab9a10c23366111
Manoel Junior <manoelj1240@gmail.com>
2023-11-21 10:49:37 -0300

    feat: add redux dependencie

---
+++ src/commentSlice.ts
@@ -0,0 +1,21 @@
+// import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+// export interface Comment {
+//   title: string;
+//   comment: string;
+// }
+// const initialState: Comment[] = []
+
+// export const commentSlice = createSlice({
+//   name: 'comment',
+//   initialState,
+//   reducers: {
+//     addComment: (state, payload) => {
+//       state = [...state, payload]
+//     }
+//   }
+// })
+
+// export const { addComment } = commentSlice.actions
+
+// export default commentSlice.reducer
---
+++ src/redux/store.ts
@@ -0,0 +1,10 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import commentReducer from "../commentSlice";
+// // ...
+
+// const store = configureStore({
+//   reducer: commentReducer,
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
